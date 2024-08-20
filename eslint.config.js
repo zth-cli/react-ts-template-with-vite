78e4259c7b41dca48f00b8a28c0fb2e4d87a14a9
ruanzhixiong <ruanzhixiong@bobandata.com>
2024-08-20 15:38:08 +0800

    refactor(eslint-config): 迁移到支持JSX的ESLint配置
    将ESLint配置从tseslint迁移到支持JSX的配置，以适应React应用程序。这包括:
    - 更新配置扩展以包含新的插件和规则。
    - 调整忽略的文件列表。
    - 采用推荐的React和TypeScript ESLint 插件设置。
    - 细化 ESLint 规则以适应项目代码实践。
    
    此更改确保代码库遵循一致的编码标准，并为JavaScript和TypeScript文件提供开箱即用的ESLint支持。

--- eslint.config.js
+++ eslint.config.js
@@ -5,27 +5,31 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintPluginPrettierRecommended],
-  files: ['**/*.{ts,tsx}'], // eslint 检测的文件，根据需要自行设置
-  ignores: ['dist', 'node_modules', '.history'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
+export default [
+  { ignores: ['dist', 'node_modules', '.history'] },
+  ...tseslint.config({
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintPluginPrettierRecommended],
+    files: ['**/*.{ts,tsx}'], // eslint 检测的文件，根据需要自行设置
 
-    'prettier/prettier': 'warn',
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off',
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
 
-    '@typescript-eslint/no-unused-expressions': 'off',
-    'react-hooks/exhaustive-deps': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn', // allow any type
-  },
-})
+      'prettier/prettier': 'warn',
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+
+      '@typescript-eslint/no-unused-expressions': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn', // allow any type
+    },
+  }),
+]
+// https://www.cnblogs.com/jsonq/p/18357943
